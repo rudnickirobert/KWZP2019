6d7c88ebb3b95dfdd65a29dc3f6c67d23f7ae921
Robert Rudnicki <robert.rudnicki@wat.edu.pl>
2019-04-08 16:34:01 +0200

    add db initializatio to startForm

--- KWZP2019/KWZP2019/Form1.cs
+++ KWZP2019/KWZP2019/Form1.cs
@@ -12,9 +12,12 @@ namespace KWZP2019
 {
     public partial class startForm : Form
     {
+        RoofingCompanyEntities db;
+
         public startForm()
         {
             InitializeComponent();
+            db = new RoofingCompanyEntities();
         }
     }
 }
