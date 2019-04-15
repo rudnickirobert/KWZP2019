bd7012b847507b5cd73c1756e812eaa72e06316a
Kuba Bartkiewicz <bartkiewiczk1@gmail.com>
2019-04-15 18:05:07 +0200

    Dodano wszytkie okienka

---
+++ KWZP2019/KWZP2019/Finances/Finances.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWZP2019
+{
+    public partial class Finances : Form
+    {
+        public Finances()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPayments_Click(object sender, EventArgs e)
+        {
+            Salaries obecnosci = new Salaries();
+            obecnosci.ShowDialog();
+        }
+
+        private void btnCountpayments_Click(object sender, EventArgs e)
+        {
+            Salariessummary urlopy = new Salariessummary();
+            urlopy.ShowDialog();
+        }
+
+        private void btnCosts_Click(object sender, EventArgs e)
+        {
+            Costs faktury = new Costs();
+            faktury.ShowDialog();
+        }
+
+        private void btnEarnings_Click(object sender, EventArgs e)
+        {
+            Profits kadry = new Profits();
+            kadry.ShowDialog();
+        }
+
+        private void btnFinancialstatement_Click(object sender, EventArgs e)
+        {
+            Allcosts wyplaty = new Allcosts();
+            wyplaty.ShowDialog();
+        }
+
+        private void btnAddinvoice_Click(object sender, EventArgs e)
+        {
+            Addfvat raporty = new Addfvat();
+            raporty.ShowDialog();
+        }
+    }
+}
