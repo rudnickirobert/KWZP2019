993efdcee89761f6835be6a08e7903a9615b9d53
Dawid Kupniewski <kupniewski.dawid@gmail.com>
2019-04-15 18:28:43 +0200

    Dodanie interfejsu graficznego

--- KWZP2019/KWZP2019/QualityControl.Designer.cs
+++ KWZP2019/KWZP2019/QualityControl.Designer.cs
@@ -28,69 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QualityControl));
             this.btnEntranceControl = new System.Windows.Forms.Button();
             this.btnOutControl = new System.Windows.Forms.Button();
             this.btnReturnMain = new System.Windows.Forms.Button();
+            this.btnOSHTraining = new System.Windows.Forms.Button();
+            this.btnDevelopment = new System.Windows.Forms.Button();
+            this.btnOSHControl = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEntranceControl
             // 
-            this.btnEntranceControl.AutoSize = true;
-            this.btnEntranceControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(92)))), ((int)(((byte)(178)))));
-            this.btnEntranceControl.FlatAppearance.BorderSize = 0;
-            this.btnEntranceControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEntranceControl.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnEntranceControl.Location = new System.Drawing.Point(32, 74);
-            this.btnEntranceControl.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.btnEntranceControl, "btnEntranceControl");
+            this.btnEntranceControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(33)))));
+            this.btnEntranceControl.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnEntranceControl.FlatAppearance.BorderSize = 3;
+            this.btnEntranceControl.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.btnEntranceControl.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(144)))), ((int)(((byte)(175)))));
+            this.btnEntranceControl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnEntranceControl.Name = "btnEntranceControl";
-            this.btnEntranceControl.Size = new System.Drawing.Size(200, 50);
-            this.btnEntranceControl.TabIndex = 2;
-            this.btnEntranceControl.Text = "Kontrola Wejściowa";
             this.btnEntranceControl.UseVisualStyleBackColor = false;
             this.btnEntranceControl.Click += new System.EventHandler(this.btnEntranceControl_Click);
             // 
             // btnOutControl
             // 
-            this.btnOutControl.AutoSize = true;
-            this.btnOutControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(92)))), ((int)(((byte)(178)))));
-            this.btnOutControl.FlatAppearance.BorderSize = 0;
-            this.btnOutControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOutControl.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnOutControl.Location = new System.Drawing.Point(32, 156);
-            this.btnOutControl.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.btnOutControl, "btnOutControl");
+            this.btnOutControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(33)))));
+            this.btnOutControl.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnOutControl.FlatAppearance.BorderSize = 3;
+            this.btnOutControl.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(144)))), ((int)(((byte)(175)))));
+            this.btnOutControl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnOutControl.Name = "btnOutControl";
-            this.btnOutControl.Size = new System.Drawing.Size(200, 50);
-            this.btnOutControl.TabIndex = 2;
-            this.btnOutControl.Text = "Kontrola Wyjściowa";
             this.btnOutControl.UseVisualStyleBackColor = false;
             this.btnOutControl.Click += new System.EventHandler(this.btnOutControl_Click);
             // 
             // btnReturnMain
             // 
-            this.btnReturnMain.AutoSize = true;
-            this.btnReturnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(92)))), ((int)(((byte)(178)))));
-            this.btnReturnMain.FlatAppearance.BorderSize = 0;
-            this.btnReturnMain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReturnMain.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnReturnMain.Location = new System.Drawing.Point(852, 43);
-            this.btnReturnMain.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.btnReturnMain, "btnReturnMain");
+            this.btnReturnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(33)))));
+            this.btnReturnMain.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnReturnMain.FlatAppearance.BorderSize = 3;
+            this.btnReturnMain.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(144)))), ((int)(((byte)(175)))));
+            this.btnReturnMain.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnReturnMain.Name = "btnReturnMain";
-            this.btnReturnMain.Size = new System.Drawing.Size(200, 50);
-            this.btnReturnMain.TabIndex = 2;
-            this.btnReturnMain.Text = "Menu Główne";
             this.btnReturnMain.UseVisualStyleBackColor = false;
             this.btnReturnMain.Click += new System.EventHandler(this.btnReturnMain_Click);
+            // 
+            // btnOSHTraining
+            // 
+            resources.ApplyResources(this.btnOSHTraining, "btnOSHTraining");
+            this.btnOSHTraining.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(33)))));
+            this.btnOSHTraining.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnOSHTraining.FlatAppearance.BorderSize = 3;
+            this.btnOSHTraining.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.btnOSHTraining.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(144)))), ((int)(((byte)(175)))));
+            this.btnOSHTraining.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnOSHTraining.Name = "btnOSHTraining";
+            this.btnOSHTraining.UseVisualStyleBackColor = false;
+            // 
+            // btnDevelopment
+            // 
+            resources.ApplyResources(this.btnDevelopment, "btnDevelopment");
+            this.btnDevelopment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(33)))));
+            this.btnDevelopment.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnDevelopment.FlatAppearance.BorderSize = 3;
+            this.btnDevelopment.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnDevelopment.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(144)))), ((int)(((byte)(175)))));
+            this.btnDevelopment.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDevelopment.Name = "btnDevelopment";
+            this.btnDevelopment.UseVisualStyleBackColor = false;
+            // 
+            // btnOSHControl
+            // 
+            resources.ApplyResources(this.btnOSHControl, "btnOSHControl");
+            this.btnOSHControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(33)))));
+            this.btnOSHControl.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnOSHControl.FlatAppearance.BorderSize = 3;
+            this.btnOSHControl.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.btnOSHControl.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(144)))), ((int)(((byte)(175)))));
+            this.btnOSHControl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnOSHControl.Name = "btnOSHControl";
+            this.btnOSHControl.UseVisualStyleBackColor = false;
             // 
             // QualityControl
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(1184, 761);
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.btnOSHControl);
+            this.Controls.Add(this.btnDevelopment);
+            this.Controls.Add(this.btnOSHTraining);
             this.Controls.Add(this.btnReturnMain);
             this.Controls.Add(this.btnOutControl);
             this.Controls.Add(this.btnEntranceControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "QualityControl";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "QualityControl";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.QualityControl_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -101,5 +133,8 @@
         private System.Windows.Forms.Button btnEntranceControl;
         private System.Windows.Forms.Button btnOutControl;
         private System.Windows.Forms.Button btnReturnMain;
+        private System.Windows.Forms.Button btnOSHTraining;
+        private System.Windows.Forms.Button btnDevelopment;
+        private System.Windows.Forms.Button btnOSHControl;
     }
 }
