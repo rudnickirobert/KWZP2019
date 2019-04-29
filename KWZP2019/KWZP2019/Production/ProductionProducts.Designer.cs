3b3875deca1a5a7a2d0aee80073aa81cbd7ef53e
marcinwat <marcin.sienicki@student.wat.edu.pl>
2019-04-29 18:27:34 +0200

    size of buttons are now equals

--- KWZP2019/KWZP2019/Production/ProductionProducts.Designer.cs
+++ KWZP2019/KWZP2019/Production/ProductionProducts.Designer.cs
@@ -34,25 +34,22 @@
             this.btDeleteProduct = new System.Windows.Forms.Button();
             this.btEnd = new System.Windows.Forms.Button();
             this.dataGridProducts = new System.Windows.Forms.DataGridView();
-            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.btRefresh = new System.Windows.Forms.Button();
-            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.btRefresh = new System.Windows.Forms.Button();
             this.idProductDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.idSemiFinishedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.productCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.idTechnologyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.inputDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btRefresh = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridProducts)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // btAddProduct
             // 
-            this.btAddProduct.Location = new System.Drawing.Point(260, 16);
-            this.btAddProduct.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btAddProduct.Location = new System.Drawing.Point(12, 27);
             this.btAddProduct.Name = "btAddProduct";
-            this.btAddProduct.Size = new System.Drawing.Size(143, 26);
+            this.btAddProduct.Size = new System.Drawing.Size(150, 30);
             this.btAddProduct.TabIndex = 0;
             this.btAddProduct.Text = "Dodaj";
             this.btAddProduct.UseVisualStyleBackColor = true;
@@ -60,10 +57,9 @@
             // 
             // btEditProduct
             // 
-            this.btEditProduct.Location = new System.Drawing.Point(419, 15);
-            this.btEditProduct.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btEditProduct.Location = new System.Drawing.Point(168, 27);
             this.btEditProduct.Name = "btEditProduct";
-            this.btEditProduct.Size = new System.Drawing.Size(143, 26);
+            this.btEditProduct.Size = new System.Drawing.Size(150, 30);
             this.btEditProduct.TabIndex = 1;
             this.btEditProduct.Text = "Edytuj";
             this.btEditProduct.UseVisualStyleBackColor = true;
@@ -71,10 +67,9 @@
             // 
             // btDeleteProduct
             // 
-            this.btDeleteProduct.Location = new System.Drawing.Point(577, 15);
-            this.btDeleteProduct.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btDeleteProduct.Location = new System.Drawing.Point(480, 27);
             this.btDeleteProduct.Name = "btDeleteProduct";
-            this.btDeleteProduct.Size = new System.Drawing.Size(143, 26);
+            this.btDeleteProduct.Size = new System.Drawing.Size(150, 30);
             this.btDeleteProduct.TabIndex = 2;
             this.btDeleteProduct.Text = "Usuń";
             this.btDeleteProduct.UseVisualStyleBackColor = true;
@@ -82,10 +77,9 @@
             // 
             // btEnd
             // 
-            this.btEnd.Location = new System.Drawing.Point(736, 15);
-            this.btEnd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btEnd.Location = new System.Drawing.Point(636, 27);
             this.btEnd.Name = "btEnd";
-            this.btEnd.Size = new System.Drawing.Size(143, 26);
+            this.btEnd.Size = new System.Drawing.Size(150, 30);
             this.btEnd.TabIndex = 3;
             this.btEnd.Text = "Koniec";
             this.btEnd.UseVisualStyleBackColor = true;
@@ -102,26 +96,11 @@
             this.idTechnologyDataGridViewTextBoxColumn,
             this.inputDateDataGridViewTextBoxColumn});
             this.dataGridProducts.DataSource = this.productBindingSource;
-            this.dataGridProducts.Location = new System.Drawing.Point(16, 49);
-            this.dataGridProducts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridProducts.Location = new System.Drawing.Point(12, 72);
             this.dataGridProducts.Name = "dataGridProducts";
-            this.dataGridProducts.Size = new System.Drawing.Size(1035, 490);
+            this.dataGridProducts.Size = new System.Drawing.Size(776, 366);
             this.dataGridProducts.TabIndex = 4;
             this.dataGridProducts.SelectionChanged += new System.EventHandler(this.dataGridProducts_SelectionChanged);
-            // 
-            // productBindingSource
-            // 
-            this.productBindingSource.DataSource = typeof(KWZP2019.Product);
-            // 
-            // btRefresh
-            // 
-            this.btRefresh.Location = new System.Drawing.Point(447, 13);
-            this.btRefresh.Name = "btRefresh";
-            this.btRefresh.Size = new System.Drawing.Size(112, 20);
-            this.btRefresh.TabIndex = 5;
-            this.btRefresh.Text = "Odśwież";
-            this.btRefresh.UseVisualStyleBackColor = true;
-            this.btRefresh.Click += new System.EventHandler(this.btRefresh_Click);
             // 
             // idProductDataGridViewTextBoxColumn
             // 
@@ -158,18 +137,32 @@
             this.inputDateDataGridViewTextBoxColumn.HeaderText = "Data dodania";
             this.inputDateDataGridViewTextBoxColumn.Name = "inputDateDataGridViewTextBoxColumn";
             // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataSource = typeof(KWZP2019.Product);
+            // 
+            // btRefresh
+            // 
+            this.btRefresh.Location = new System.Drawing.Point(324, 27);
+            this.btRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btRefresh.Name = "btRefresh";
+            this.btRefresh.Size = new System.Drawing.Size(150, 30);
+            this.btRefresh.TabIndex = 5;
+            this.btRefresh.Text = "Odśwież";
+            this.btRefresh.UseVisualStyleBackColor = true;
+            this.btRefresh.Click += new System.EventHandler(this.btRefresh_Click);
+            // 
             // ProductionProducts
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btRefresh);
             this.Controls.Add(this.dataGridProducts);
             this.Controls.Add(this.btEnd);
             this.Controls.Add(this.btDeleteProduct);
             this.Controls.Add(this.btEditProduct);
             this.Controls.Add(this.btAddProduct);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ProductionProducts";
             this.Text = "Produkty";
             this.Load += new System.EventHandler(this.ProductionProducts_Load);
