87ddf91cdc469e716f4d60a874226602a70863b0
Michał Kujda <kujda6@hotmail.com>
2019-04-29 18:31:54 +0200

    HR_Finances_Fixed_Compilation

--- KWZP2019/KWZP2019/Education.Designer.cs
+++ KWZP2019/KWZP2019/Education.Designer.cs
@@ -11,15 +11,7 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
+    
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,10 +20,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Education";
+
         }
 
         #endregion
