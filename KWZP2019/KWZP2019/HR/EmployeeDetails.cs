d5488b608c2a01a407e6e112478937d530850f88
Kuba Bartkiewicz <bartkiewiczk1@gmail.com>
2019-05-03 14:11:15 +0200

    code formating

--- KWZP2019/KWZP2019/HR/EmployeeDetails.cs
+++ KWZP2019/KWZP2019/HR/EmployeeDetails.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace KWZP2019
 {
@@ -70,7 +69,7 @@ namespace KWZP2019
                         employeeDetailsSelect.HealTestDate,
                         employeeDetailsSelect.WorkplaceTrainingDate,
                         employeeDetailsSelect.Date
-            }).ToList();
+                }).ToList();
         }
     }
 }
