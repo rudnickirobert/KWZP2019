0a852901159af8ed4185780d65014accb4e10ef7
mateusz1kazana <matkazana@gmail.com>
2019-05-04 20:27:43 +0200

    TOP 10 PERCENT DELETED

--- RoofingCompany.sql
+++ RoofingCompany.sql
@@ -769,7 +769,7 @@ GO
 GO
 CREATE VIEW vOrder
 AS
-SELECT TOP 10 PERCENT Part.PartName as [Nazwa części], 
+SELECT Part.PartName as [Nazwa części], 
 PartType.partType as [Typ części], 
 Unit.UnitName as [Jednostka], 
 Part.QuantityWarehouse as [Stan magazynowy]
@@ -801,7 +801,7 @@ WHERE (DepartmentName = 'Logistyka');
 GO 
 CREATE VIEW vEmployeeSD
 AS
-SELECT TOP 10 PERCENT Maintenance.MaintenanceNr as [Nr Obsługi], Maintenance.DateAcceptOrder as [Data przyjęcia], 
+SELECT Maintenance.MaintenanceNr as [Nr Obsługi], Maintenance.DateAcceptOrder as [Data przyjęcia], 
 Part.PartName as [Nazwa części], MaintPart.PartQuantity as [Ilość], Unit.UnitName as [Jednostka]
 FROM Unit INNER JOIN (Maintenance INNER JOIN (Part INNER JOIN MaintPart 
 ON Part.IdPart = MaintPart.IdPart) 
