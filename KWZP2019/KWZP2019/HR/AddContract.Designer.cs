b230d85dfbae357f64eb43ed3f9a5c157caaacb5
Kuba Bartkiewicz <bartkiewiczk1@gmail.com>
2019-05-07 14:20:24 +0200

    AddContract form fixed

--- KWZP2019/KWZP2019/HR/AddContract.Designer.cs
+++ KWZP2019/KWZP2019/HR/AddContract.Designer.cs
@@ -35,53 +35,55 @@
             this.cbPosition = new System.Windows.Forms.ComboBox();
             this.dtpWorkplaceTrainingDate = new System.Windows.Forms.DateTimePicker();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.cbDepartmentList = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dtpStartDate
             // 
-            this.dtpStartDate.Location = new System.Drawing.Point(56, 119);
+            this.dtpStartDate.Location = new System.Drawing.Point(44, 94);
             this.dtpStartDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpStartDate.Name = "dtpStartDate";
-            this.dtpStartDate.Size = new System.Drawing.Size(273, 22);
+            this.dtpStartDate.Size = new System.Drawing.Size(265, 22);
             this.dtpStartDate.TabIndex = 0;
             // 
             // cbEmployeeList
             // 
             this.cbEmployeeList.FormattingEnabled = true;
-            this.cbEmployeeList.Location = new System.Drawing.Point(172, 55);
+            this.cbEmployeeList.Location = new System.Drawing.Point(44, 34);
             this.cbEmployeeList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbEmployeeList.Name = "cbEmployeeList";
-            this.cbEmployeeList.Size = new System.Drawing.Size(121, 24);
+            this.cbEmployeeList.Size = new System.Drawing.Size(265, 24);
             this.cbEmployeeList.TabIndex = 1;
             // 
             // tbSalary
             // 
-            this.tbSalary.Location = new System.Drawing.Point(268, 215);
+            this.tbSalary.Location = new System.Drawing.Point(44, 150);
             this.tbSalary.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbSalary.Name = "tbSalary";
-            this.tbSalary.Size = new System.Drawing.Size(100, 22);
+            this.tbSalary.Size = new System.Drawing.Size(265, 22);
             this.tbSalary.TabIndex = 2;
             // 
             // dtpEndDate
             // 
-            this.dtpEndDate.Location = new System.Drawing.Point(395, 122);
+            this.dtpEndDate.Location = new System.Drawing.Point(44, 122);
             this.dtpEndDate.Margin = new System.Windows.Forms.Padding(4);
             this.dtpEndDate.Name = "dtpEndDate";
-            this.dtpEndDate.Size = new System.Drawing.Size(297, 22);
+            this.dtpEndDate.Size = new System.Drawing.Size(265, 22);
             this.dtpEndDate.TabIndex = 3;
             // 
             // cbPosition
             // 
             this.cbPosition.FormattingEnabled = true;
-            this.cbPosition.Location = new System.Drawing.Point(427, 214);
+            this.cbPosition.Location = new System.Drawing.Point(44, 64);
             this.cbPosition.Margin = new System.Windows.Forms.Padding(4);
             this.cbPosition.Name = "cbPosition";
-            this.cbPosition.Size = new System.Drawing.Size(160, 24);
+            this.cbPosition.Size = new System.Drawing.Size(265, 24);
             this.cbPosition.TabIndex = 4;
             // 
             // dtpWorkplaceTrainingDate
             // 
-            this.dtpWorkplaceTrainingDate.Location = new System.Drawing.Point(427, 288);
+            this.dtpWorkplaceTrainingDate.Location = new System.Drawing.Point(44, 178);
             this.dtpWorkplaceTrainingDate.Margin = new System.Windows.Forms.Padding(4);
             this.dtpWorkplaceTrainingDate.Name = "dtpWorkplaceTrainingDate";
             this.dtpWorkplaceTrainingDate.Size = new System.Drawing.Size(265, 22);
@@ -98,11 +100,33 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // cbDepartmentList
+            // 
+            this.cbDepartmentList.FormattingEnabled = true;
+            this.cbDepartmentList.Location = new System.Drawing.Point(480, 124);
+            this.cbDepartmentList.Margin = new System.Windows.Forms.Padding(4);
+            this.cbDepartmentList.Name = "cbDepartmentList";
+            this.cbDepartmentList.Size = new System.Drawing.Size(265, 24);
+            this.cbDepartmentList.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(388, 372);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 38);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Wyczyść";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
             // AddContract
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.cbDepartmentList);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.dtpWorkplaceTrainingDate);
             this.Controls.Add(this.cbPosition);
@@ -128,5 +152,7 @@
         private System.Windows.Forms.ComboBox cbPosition;
         private System.Windows.Forms.DateTimePicker dtpWorkplaceTrainingDate;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cbDepartmentList;
+        private System.Windows.Forms.Button btnClear;
     }
 }
