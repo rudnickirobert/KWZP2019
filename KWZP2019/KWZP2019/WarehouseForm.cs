ac6062f36d7dfb72f890d9697d7cbe67a5e18002
Bartłomiej Łotys <bartlomiejlotys@gmail.com>
2019-05-08 22:05:45 +0200

    Poprawiony magazyn

--- KWZP2019/KWZP2019/WarehouseForm.cs
+++ KWZP2019/KWZP2019/WarehouseForm.cs
@@ -23,7 +23,9 @@ namespace KWZP2019
         private void WarehouseForm_Load(object sender, EventArgs e)
         {
             entraceWarehouseDgv.DataSource = db.vInputMagazines.ToList();
+            entraceWarehouseDgv.Columns["Numer"].Visible = false;
             outWarehouseDgv.DataSource = db.vOutputMagazines.ToList();
+            outWarehouseDgv.Columns["Numer"].Visible = false;
         }
         private void backBtn_Click(object sender, EventArgs e)
         {
@@ -43,6 +45,7 @@ namespace KWZP2019
             if (semiFinishedCodeTb.Text.Trim() == "")
             {
                 entraceWarehouseDgv.DataSource = db.vInputMagazines.ToList();
+                entraceWarehouseDgv.Columns["Numer"].Visible = false;
             }
             else
             {
@@ -50,6 +53,7 @@ namespace KWZP2019
                                                   where
                                                    db.Kod_Produktu.Contains(semiFinishedCodeTb.Text.Trim())
                                                   select db).ToList();
+                entraceWarehouseDgv.Columns["Numer"].Visible = false;
             }
         }
         private void productCodeTb_TextChanged(object sender, EventArgs e)
@@ -57,6 +61,7 @@ namespace KWZP2019
             if (productCodeTb.Text.Trim() == "")
             {
                 outWarehouseDgv.DataSource = db.vOutputMagazines.ToList();
+                outWarehouseDgv.Columns["Numer"].Visible = false;
             }
             else
             {
@@ -64,6 +69,7 @@ namespace KWZP2019
                                               where
                                                db.Kod_produktu.Contains(productCodeTb.Text.Trim())
                                               select db).ToList();
+                outWarehouseDgv.Columns["Numer"].Visible = false;
             }
         }
     }
